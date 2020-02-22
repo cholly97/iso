189a80935731c9e3904b19b0484a331d79bf64a2
Charles Meng <mengcharles@gmail.com>
2020-02-22 11:53:45 -0500

    placeholder world, IO and timeupdate

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,26 @@ module Lib
     ) where
 
 import           Graphics.Gloss
+import           Graphics.Gloss.Interface.IO.Game
+
+fps = 60
+bgColor = white
+type World = Float
+
+initWorld :: World
+initWorld = 42.0
+
+displayWorld :: World -> IO Picture
+displayWorld w = return (Circle w)
+
+handleInputs :: Event -> World -> IO World
+handleInputs _ _ = return initWorld
+
+timeUpdate :: Float -> World -> IO World
+timeUpdate dt w = return (w + dt)
+
+window :: Display
+window = FullScreen
 
 someFunc :: IO ()
-someFunc = display (InWindow "Nice Window" (200, 200) (10, 10)) white (Circle 80)
+someFunc = playIO window bgColor fps initWorld displayWorld handleInputs timeUpdate
