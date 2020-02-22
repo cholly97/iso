ce82f0467d907cdc1a9bd77421c905b39bea9805
Charles Meng <mengcharles@gmail.com>
2020-02-22 13:51:08 -0500

    basic World definition using Control.Lens to write inside World record

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,28 +1,55 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  )
+where
 
+import           Control.Lens
 import           Graphics.Gloss
 import           Graphics.Gloss.Interface.IO.Game
 
 fps = 60
+
 bgColor = white
-type World = Float
+
+data LimitPoint
+  = Point Point
+  | Angle Float
+
+type Triple a = (a, a, a)
+
+data Grid = Grid
+  { _spacing :: Float
+  , _limitPoints :: Triple LimitPoint
+  }
+
+data World = World
+  { _grid :: Grid
+  }
+
+$(makeLenses ''World)
+$(makeLenses ''Grid)
 
 initWorld :: World
-initWorld = 42.0
+initWorld = World
+  { _grid = Grid { _spacing     = 42.0
+                 , _limitPoints = (Angle 1.0, Angle 2.0, Point (3.0, 4.0))
+                 }
+  }
 
 displayWorld :: World -> IO Picture
-displayWorld w = return (Circle w)
+displayWorld = return . Circle . _spacing . _grid
 
 handleInputs :: Event -> World -> IO World
 handleInputs _ _ = return initWorld
 
 timeUpdate :: Float -> World -> IO World
-timeUpdate dt w = return (w + dt)
+timeUpdate = (return .) . (grid . spacing %~) . (+) . (* 10)
+-- timeUpdate dt w = return ((over (grid . spacing) (+ (dt * 10))) w)
 
 window :: Display
 window = FullScreen
 
 someFunc :: IO ()
-someFunc = playIO window bgColor fps initWorld displayWorld handleInputs timeUpdate
+someFunc =
+  playIO window bgColor fps initWorld displayWorld handleInputs timeUpdate
