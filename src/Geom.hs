3e1a0de1ebb3ab5199b90362fc95ad9591e9ecbf
Charles Meng <mengcharles@gmail.com>
2020-03-01 06:20:36 -0500

    small visual changes

--- src/Geom.hs
+++ src/Geom.hs
@@ -18,14 +18,13 @@ projectVV = dotV
 anglePP :: Point -> Point -> Float
 anglePP p q = argV (p Pt.- q) `mod'` pi
 
-
 translateP :: Point -> Picture -> Picture
 translateP = uncurry Translate
 
 rayPA :: Bounds -> Point -> Float -> Picture
 -- draw ray based on point and angle
 rayPA (w, h) p a =
-  translateP p . (Rotate (radToDeg (-a))) $ Line [origin, (w + h, 0)]
+  translateP p . (Rotate . radToDeg $ -a) $ Line [origin, (w + h, 0)]
 
 lineAND :: Bounds -> Float -> Vector -> Float -> Picture
 -- draw line based on angle, normal vector, and distance
@@ -34,4 +33,4 @@ lineAND b a v d = linePA b (d Pt.* v) a
 linePA :: Bounds -> Point -> Float -> Picture
 -- draw line based on point and angle
 linePA (w, h) p a =
-  translateP p . (Rotate (radToDeg (-a))) $ Line [(-w - h, 0), (w + h, 0)]
+  translateP p . (Rotate . radToDeg $ -a) $ Line [(-w - h, 0), (w + h, 0)]
