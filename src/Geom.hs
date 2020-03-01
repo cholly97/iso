10eb0d3841b23e583fc7ef4ee1f42886e4a49ede
Charles Meng <mengcharles@gmail.com>
2020-03-01 03:12:04 -0500

    refactored geometry functions into new file

---
+++ src/Geom.hs
@@ -0,0 +1,39 @@
+module Geom where
+
+import           Data.Fixed
+import           Graphics.Gloss
+import           Graphics.Gloss.Data.Vector
+import qualified Graphics.Gloss.Data.Point.Arithmetic
+                                               as Pt
+import           Graphics.Gloss.Geometry.Angle
+
+type Bounds = (Float, Float)
+
+type Angle = Float
+
+origin :: Point
+origin = (0, 0)
+
+projectVV :: Vector -> Vector -> Float
+projectVV = dotV
+
+anglePP :: Point -> Point -> Angle
+anglePP p q = argV (p Pt.- q) `mod'` pi
+
+
+translateP :: Point -> Picture -> Picture
+translateP = uncurry Translate
+
+rayPA :: Bounds -> Point -> Float -> Picture
+-- draw ray based on point and angle
+rayPA (w, h) p a =
+  translateP p . (Rotate (radToDeg (-a))) $ Line [origin, (w + h, 0)]
+
+lineAND :: Bounds -> Angle -> Vector -> Float -> Picture
+-- draw line based on angle, normal vector, and distance
+lineAND b a v d = linePA b (d Pt.* v) a
+
+linePA :: Bounds -> Point -> Angle -> Picture
+-- draw line based on point and angle
+linePA (w, h) p a =
+  translateP p . (Rotate (radToDeg (-a))) $ Line [(-w - h, 0), (w + h, 0)]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,30 +1,22 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Lib where
 
+import           Geom
 import           Data.Maybe
 import qualified Data.Set                      as Set
 import           Data.Monoid
-import           Data.Fixed
 import           Control.Lens
 import           Control.Arrow
 import           Control.Monad
 import           Graphics.Gloss
 import           Graphics.Gloss.Data.Vector
-import           Graphics.Gloss.Geometry.Angle
 import           Graphics.Gloss.Interface.IO.Game
 import           Graphics.Gloss.Interface.Environment
-import qualified Graphics.Gloss.Data.Point.Arithmetic
-                                               as Pt
 
-type Bounds = (Float, Float)
-
-type Angle = Float
 
 data Limit
   = Infinite {angle :: Angle, normal :: Vector, dists :: Set.Set Float}
   | Finite {point :: Point, angles :: Set.Set Angle}
-
-type Triple a = (a, a, a)
 
 data Grid = Grid
   { _spacing :: Float
@@ -39,9 +31,6 @@ data World = World
 
 $(makeLenses ''World)
 $(makeLenses ''Grid)
-
-origin :: Point
-origin = (0, 0)
 
 initWorld :: World
 initWorld = World
@@ -70,12 +59,6 @@ drawGrid world =
         ]
     <*> (world ^. grid . limits)
 
-projectVV :: Vector -> Vector -> Float
-projectVV = dotV
-
-anglePP :: Point -> Point -> Angle
-anglePP p q = argV (p Pt.- q) `mod'` pi
-
 drawClosestGridLines :: Bounds -> Point -> Limit -> Picture
 drawClosestGridLines b q (Infinite a v ds) =
   Pictures $ [Color red . lineAND b a v] <*> lookupLGE (projectVV q v) ds
@@ -101,23 +84,6 @@ drawGridLines :: Bounds -> Limit -> Picture
 drawGridLines b (Infinite a v ds) =
   Pictures $ [lineAND b a v] <*> Set.toList ds
 drawGridLines b (Finite p as) = Pictures $ [linePA b p] <*> Set.toList as
-
-translateP :: Point -> Picture -> Picture
-translateP = uncurry Translate
-
-rayPA :: Bounds -> Point -> Float -> Picture
--- draw ray based on point and angle
-rayPA (w, h) p a =
-  translateP p . (Rotate (radToDeg (-a))) $ Line [origin, (w + h, 0)]
-
-lineAND :: Bounds -> Angle -> Vector -> Float -> Picture
--- draw line based on angle, normal vector, and distance
-lineAND b a v d = linePA b (d Pt.* v) a
-
-linePA :: Bounds -> Point -> Angle -> Picture
--- draw line based on point and angle
-linePA (w, h) p a =
-  translateP p . (Rotate (radToDeg (-a))) $ Line [(-w - h, 0), (w + h, 0)]
 
 addLine :: Point -> Limit -> Limit
 addLine q (Infinite a v ds) = Infinite a v (Set.insert (projectVV q v) ds)
