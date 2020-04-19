11dafc2a9eae2800bf1f3ddf2d2f6d995bb61d4e
Charles Meng <mengcharles@gmail.com>
2020-04-20 01:28:56 +0800

    made new version of finger using partial functions (failing with error) instead of Maybe monad

--- src/Finger.hs
+++ src/Finger.hs
@@ -16,124 +16,229 @@ instance Ord a => Ord (Range a) where
   compare _          NegInf     = GT
   compare (Finite x) (Finite y) = compare x y
 
+maxInterval :: Interval a
+maxInterval = (NegInf, PosInf)
+
 inRange :: Ord a => a -> (Range a, Range a) -> Bool
 inRange k (lo, hi) = (compare lo (Finite k), compare hi (Finite k)) == (LT, GT)
 
 type Interval a = (Range a, Range a)
-data Parent t a = L a (t a) (Range a) | R a (t a) (Range a)
+data Parent t a = L (t a) (Range a) | R (t a) (Range a)
 type Finger t a = (TreeView t a, Interval a, [Parent t a])
 
-class BST bst => FingerBST bst where
-  -- W/S - O(1)
-  lift :: Finger bst a -> Maybe a
-  lift (t, _, _) = getValue t
+lift :: Finger t a -> TreeView t a
+lift (t, _, _) = t
+interval :: Finger t a -> Interval a
+interval (_, i, _) = i
+parents :: Finger t a -> [Parent t a]
+parents (_, _, ps) = ps
 
-  -- moving the finger around
-  -- W/S - O(1)
-  childL, childR, parent :: Finger bst a -> Maybe (Finger bst a)
-  childL (t, (lo, hi), ps) = case t of
-    Leaf          -> Nothing
-    Node k (l, r) -> Just (expose l, (lo, Finite k), L k r hi : ps)
-  childR (t, (lo, hi), ps) = case t of
-    Leaf          -> Nothing
-    Node k (l, r) -> Just (expose r, (Finite k, hi), R k l lo : ps)
-  parent (t, (lo, hi), p : ps) = case p of
-    L k r hi' -> Just (Node k (unexpose t, r), (lo, hi'), ps)
-    R k l lo' -> Just (Node k (l, unexpose t), (lo', hi), ps)
-  parent _ = Nothing
+class BST bst => FingerBST bst where
   -- constructing fingers
-  -- root: W/S - O(1)
-  -- inf, sup: W/S - (lg |t|)
-  root, inf, sup :: bst a -> Maybe (Finger bst a)
-  root b = case t of
-    Leaf -> Nothing
-    _ -> Just (t, (NegInf, PosInf), [])
-    where t = expose b
-  inf = doToRoot inf'
-  sup = doToRoot sup'
-  doToRoot :: (Finger bst a -> Finger bst a) -> bst a -> Maybe (Finger bst a)
-  doToRoot = (root >>-->)
-  inf', sup' :: Finger bst a -> Finger bst a
-  inf' = doUntilNothing childL >-> parent >-> tryFromMaybe "empty tree"
-  sup' = doUntilNothing childR >-> parent >-> tryFromMaybe "empty tree"
+  point :: bst a -> Finger bst a
+  point b = (expose b, maxInterval, [])
   -- reconstructing trees
+  -- W/S - O(1)
+  unPoint :: Finger bst a -> bst a
+  unPoint = lift >-> unexpose
   -- W/S - O(lg |t|)
   reconstruct :: Finger bst a -> bst a
   reconstruct f@(t, _, _) = maybe (unexpose t) reconstruct $ parent f
-  -- rotations
+
+  -- moving the finger around, fail if can't move
   -- W/S - O(1)
-  rotateR :: Finger bst a -> Maybe (Finger bst a)
+  parent, childL, childR :: Finger bst a -> Maybe (Finger bst a)
+  parent (t, (lo, Finite k), L r hi' : ps) =
+    Just (Node k (unexpose t, r), (lo, hi'), ps)
+  parent (t, (Finite k, hi), R l lo' : ps) =
+    Just (Node k (l, unexpose t), (lo', hi), ps)
+  parent _ = Nothing
+  childL (t, (lo, hi), ps) = case t of
+    Leaf          -> Nothing
+    Node k (l, r) -> Just (expose l, (lo, Finite k), L r hi : ps)
+  childR (t, (lo, hi), ps) = case t of
+    Leaf          -> Nothing
+    Node k (l, r) -> Just (expose r, (Finite k, hi), R l lo : ps)
+
+  -- bulk moves, fail if tree is empty
+  -- root: W/S - O(1)
+  -- minimal, maximal: W/S - (lg |t|)
+  root, minimal, maximal :: Finger bst a -> Maybe (Finger bst a)
+  root f@(Node {}, _, []) = Just f
+  root f = parent f >>= root
+  minimal f@(_, (NegInf, _), _) = inf f
+  minimal f = parent f >>= minimal
+  maximal f@(_, (_, PosInf), _) = sup f
+  maximal f = parent f >>= maximal
+  -- minimal/maximal in subtree, unless Leaf node, in which case return parent
+  inf, sup :: Finger bst a -> Maybe (Finger bst a)
+  inf = doUntilNothing childL >-> parent
+  sup = doUntilNothing childR >-> parent
+
+  -- rotations, fail if either of two nodes are Leafs
+  -- W/S - O(1)
+  rotateR, rotateL :: Finger bst a -> Maybe (Finger bst a)
   rotateR (Leaf, _, _) = Nothing
   rotateR (Node k (l, r), i, ps) =
     case expose l of
       Leaf -> Nothing
       Node lk (ll, lr) -> Just (Node lk (ll, r'), i, ps)
         where r' = unexpose $ Node k (lr, r)
-  rotateL :: Finger bst a -> Maybe (Finger bst a)
   rotateL (Leaf, _, _) = Nothing
   rotateL (Node k (l, r), i, ps) =
     case expose r of
       Leaf -> Nothing
       Node rk (rl, rr) -> Just (Node rk (l', rr), i, ps)
         where l' = unexpose $ Node k (l, rl)
-  -- iterating using fingers
+
+  -- iterating using fingers, fail if doesn't exist
   -- W/S - O(1) expected
-  next, prev :: Ord a => Finger bst a -> Maybe (Finger bst a)
-  next = adj inf' childR parentR
-  prev = adj sup' childL parentL
-  adj :: Ord a =>
-    (Finger bst a -> Finger bst a) ->
-    (Finger bst a -> Maybe (Finger bst a)) ->
-    (Finger bst a -> Maybe (Finger bst a)) ->
-    Finger bst a -> Maybe (Finger bst a)
-  adj b' c p f =
-    if isNothing child then p f else child where child = c f >-> b'
+  prev, next :: Ord a => Finger bst a -> Maybe (Finger bst a)
+  prev f@(Leaf, _, _) = parentL f
+  prev f@(Node k (l, _), _, _) = case expose l of
+    Leaf -> parentL f
+    _ -> childL f >>= sup
+  next f@(Leaf, _, _) = parentR f
+  next f@(Node k (_, r), _, _) = case expose r of
+    Leaf -> parentR f
+    _ -> childR f >>= inf
   parentL, parentR :: Finger bst a -> Maybe (Finger bst a)
-  parentL f@(_, _, p : _) = case p of
+  parentL f@(_, (_, Finite _), p : _) = case p of
     L {} -> parent f
     R {} -> parent f >>= parentL
   parentL _ = Nothing
-  parentR f@(_, _, p : _) = case p of
+  parentR f@(_, (Finite _, _), p : _) = case p of
     L {} -> parent f >>= parentR
     R {} -> parent f
   parentR _ = Nothing
 
-  -- searching a tree
+  -- searching a tree, returns Leaf finger if not in tree
   -- W/S - O(lg |t|) expected
   search :: Ord a => a -> bst a -> Maybe (Finger bst a)
-  search k = root >>=> searchFrom k
+  search k = point >-> searchFrom k
   -- searching from a finger
   -- W/S - O(lg(j - i + 1)) expected
   -- where i, j are ranks of the start and end keys
   searchFrom :: Ord a => a -> Finger bst a -> Maybe (Finger bst a)
   searchFrom k = sf where
     sf f@(tv, i, _) = case tv of
-      Leaf -> Nothing
+      Leaf -> if inRange k i then Just f else parent f >>= sf
       Node k' _ -> if inRange k i
         then case compare k k' of
           EQ -> Just f
-          LT -> sf =<< childL f
-          GT -> sf =<< childR f
-        else sf =<< parent f
-  -- alternate search where a Leaf finger is returned instead of Nothing
-  -- W/S - O(lg |t|) expected
-  search' :: Ord a => a -> bst a -> Finger bst a
-  search' k t = maybe (Leaf, (NegInf, PosInf), []) (searchFrom' k) $ root t
-  -- W/S - O(lg(j - i + 1)) expected
-  -- where i, j are ranks of the start and end keys
-  searchFrom' :: Ord a => a -> Finger bst a -> Finger bst a
-  searchFrom' k = sf where
-    sf f@(tv, i, _) = case tv of
-      Leaf -> f
-      Node k' _ -> if inRange k i
-        then case compare k k' of
-          EQ -> f
-          LT -> tryMaybe "bad implementation of childL" sf $ childL f
-          GT -> tryMaybe "bad implementation of childR" sf $ childR f
-        else tryMaybe "malformed root finger" sf $ parent f
+          LT -> childL f >>= sf
+          GT -> childR f >>= sf
+        else parent f >>= sf
 
   -- last non-leaf node accessed, unless tree is empty
   -- W/S - O(1)
-  lastAccessed :: Finger bst a -> Finger bst a
-  lastAccessed f@(Leaf, _, _:_) = tryFromMaybe "no parent" $ parent f
-  lastAccessed f = f
+  lastAccessed :: Finger bst a -> Maybe (Finger bst a)
+  lastAccessed f@(Leaf, _, _) = parent f
+  lastAccessed f = Just f
+
+-- version that fails with error instead of using the Maybe monad
+class BST bst => FingerBST' bst where
+  -- constructing fingers
+  point' :: bst a -> Finger bst a
+  point' b = (expose b, maxInterval, [])
+  -- reconstructing trees
+  -- W/S - O(1)
+  unPoint' :: Finger bst a -> bst a
+  unPoint' = lift >-> unexpose
+  -- W/S - O(lg |t|)
+  reconstruct' :: Finger bst a -> bst a
+  reconstruct' f@(_, _, []) = unPoint' f
+  reconstruct' f            = parent' f -< reconstruct'
+
+  -- moving the finger around, fail if can't move
+  -- W/S - O(1)
+  parent', childL', childR':: Finger bst a -> Finger bst a
+  parent' (t, (lo, Finite k), L r hi' : ps) =
+    (Node k (unexpose t, r), (lo, hi'), ps)
+  parent' (t, (Finite k, hi), R l lo' : ps) =
+    (Node k (l, unexpose t), (lo', hi), ps)
+  parent' _ = error "root"
+  childL' (t, (lo, hi), ps) = case t of
+    Leaf          -> error "leaf"
+    Node k (l, r) -> (expose l, (lo, Finite k), L r hi : ps)
+  childR' (t, (lo, hi), ps) = case t of
+    Leaf          -> error "leaf"
+    Node k (l, r) -> (expose r, (Finite k, hi), R l lo : ps)
+
+  -- bulk moves, fail if tree is empty
+  -- root: W/S - O(1)
+  -- minimal, maximal: W/S - (lg |t|)
+  root', minimal', maximal' :: Finger bst a -> Finger bst a
+  root' f@(Node {}, _, []) = f
+  root' f = parent' f -< root'
+  minimal' f@(_, (NegInf, _), _) = inf' f
+  minimal' f = parent' f -< minimal'
+  maximal' f@(_, (_, PosInf), _) = sup' f
+  maximal' f = parent' f -< maximal'
+  -- minimal/maximal in subtree, unless Leaf node, in which case return parent
+  inf', sup' :: Finger bst a -> Finger bst a
+  inf' f@(Leaf, _, _) = parent' f
+  inf' f = childL' f -< inf'
+  sup' f@(Leaf, _, _) = parent' f
+  sup' f = childR' f -< sup'
+
+  -- rotations, fail if either of two nodes are Leafs
+  -- W/S - O(1)
+  rotateL', rotateR' :: Finger bst a -> Finger bst a
+  rotateL' (Leaf, _, _) = error "leaf"
+  rotateL' (Node k (l, r), i, ps) =
+    case expose r of
+      Leaf -> error "right leaf"
+      Node rk (rl, rr) -> (Node rk (l', rr), i, ps)
+        where l' = unexpose $ Node k (l, rl)
+  rotateR' (Leaf, _, _) = error "leaf"
+  rotateR' (Node k (l, r), i, ps) =
+    case expose l of
+      Leaf -> error "left leaf"
+      Node lk (ll, lr) -> (Node lk (ll, r'), i, ps)
+        where r' = unexpose $ Node k (lr, r)
+
+  -- iterating using fingers, fail if maximal/minimal Leaf/Node
+  -- W/S - O(1) expected
+  prev', next' :: Ord a => Finger bst a -> Finger bst a
+  prev' f@(Leaf, _, _) = parentL' f
+  prev' f@(Node k (l, _), _, _) = case expose l of
+    Leaf -> parentL' f
+    _ -> sup' . childL' $ f
+  next' f@(Leaf, _, _) = parentR' f
+  next' f@(Node k (_, r), _, _) = case expose r of
+    Leaf -> parentR' f
+    _ -> inf' . childR' $ f
+  parentL', parentR' :: Finger bst a -> Finger bst a
+  parentL' f@(_, (_, Finite _), p : _) = case p of
+    L {} -> parent' f
+    R {} -> parent' f -< parentL'
+  parentL' _ = error "no left parent"
+  parentR' f@(_, (Finite _, _), p : _) = case p of
+    L {} -> parent' f -< parentL'
+    R {} -> parent' f
+  parentR' _ = error "no right parent"
+
+  -- searching a tree, returns Leaf finger if not in tree
+  -- W/S - O(lg |t|) expected
+  search' :: Ord a => a -> bst a -> Finger bst a
+  search' k = point' >-> searchFrom' k
+  -- searching from a finger
+  -- W/S - O(lg(j - i + 1)) expected
+  -- where i, j are ranks of the start and end keys
+  searchFrom' :: Ord a => a -> Finger bst a -> Finger bst a
+  searchFrom' k = sf' where
+    sf' f@(tv, i, _) = case tv of
+      Leaf -> if inRange k i then f else parent' f -< sf'
+      Node k' _ -> if inRange k i
+        then case compare k k' of
+          EQ -> f
+          LT -> childL' f -< sf'
+          GT -> childR' f -< sf'
+        else parent' f -< sf'
+
+  -- last non-leaf node accessed, unless tree is empty
+  -- W/S - O(1)
+  lastAccessed' :: Finger bst a -> Finger bst a
+  lastAccessed' f@(Leaf, _, _) = parent' f
+  lastAccessed' f = f
--- src/Splay.hs
+++ src/Splay.hs
@@ -19,36 +19,54 @@ instance BST SplayTree where
   singleton k = N k (E, E)
 
 instance FingerBST SplayTree
+instance FingerBST' SplayTree
 
 instance Show a => Show (SplayTree a) where
   show = toString2d 1
 
 -- W/S - O(1)
 splayOp :: Finger SplayTree a -> Maybe (Finger SplayTree a)
-splayOp f@(_, _, ps) = case ps of
-  L{} : L{} : _ -> parent f >>= parent >>= rotateR >>= rotateR
-  R{} : R{} : _ -> parent f >>= parent >>= rotateL >>= rotateL
-  L{} : R{} : _ -> parent f >>= rotateR >>= parent >>= rotateL
-  R{} : L{} : _ -> parent f >>= rotateL >>= parent >>= rotateR
-  L{}       : _ -> parent f >>= rotateR
-  R{}       : _ -> parent f >>= rotateL
-  []            -> Nothing
+splayOp f@(_, _, ps) = op f
+ where
+  op = case ps of
+    L{} : L{} : _ -> parent >>=> parent >>=> rotateR >>=> rotateR
+    R{} : R{} : _ -> parent >>=> parent >>=> rotateL >>=> rotateL
+    L{} : R{} : _ -> parent >>=> rotateR >>=> parent >>=> rotateL
+    R{} : L{} : _ -> parent >>=> rotateL >>=> parent >>=> rotateR
+    L{}       : _ -> parent >>=> rotateR
+    R{}       : _ -> parent >>=> rotateL
+    []            -> const Nothing
+splayOp' :: Finger SplayTree a -> Finger SplayTree a
+splayOp' f@(_, _, ps) = op f
+ where
+  op = case ps of
+    L{} : L{} : _ -> parent' >-> parent' >-> rotateR' >-> rotateR'
+    R{} : R{} : _ -> parent' >-> parent' >-> rotateL' >-> rotateL'
+    L{} : R{} : _ -> parent' >-> rotateR' >-> parent' >-> rotateL'
+    R{} : L{} : _ -> parent' >-> rotateL' >-> parent' >-> rotateR'
+    L{}       : _ -> parent' >-> rotateR'
+    R{}       : _ -> parent' >-> rotateL'
+    []            -> error "root"
 -- W/S - O(lg |t|) expected
-splay :: Finger SplayTree a -> Finger SplayTree a
+splay, splay' :: Finger SplayTree a -> Finger SplayTree a
 splay = doUntilNothing splayOp
+-- version that uses splayOp'
+splay' f@(_, _, []) = f
+splay' f            = splayOp' f -< splay'
 
 instance SelfBalancing SplayTree where
-  join t1 t2 = maybe t2 join' $ sup t1
+  join t1 t2 = case expose t1 of
+    Leaf -> t2
+    _    -> point' t1 -< maximal' -< joinWitht2
    where
-    join'    = splay >-> replaceR >-> reconstruct
-    replaceR = set >- _1 . Trees.children . _2 >- t2
+    joinWitht2 = splay' >-> replaceRt2 >-> unexpose
+    replaceRt2 = set >- Trees.children . _2 >- t2 <-< lift
     -- partial function because splaying largest element to root
     -- should always result in empty right child, and no parent
-    -- replaceR (Node k (l, empty), (NegInf, PosInf), []) =
-      -- (Node k (l, t2), (NegInf, PosInf), [])
+    -- replaceR (Node k (l, _), (NegInf, PosInf), []) = Node k (l, t2)
     -- replaceR _ = error "bad implementation of splay and/or sup"
 
-  split k t = case splay . lastAccessed $ search' k t of
+  split k t = case search' k t -< lastAccessed' -< splay' of
     (Leaf          , _, _) -> (False, empty, empty)
     (Node k' (l, r), _, _) -> case compare k k' of
       EQ -> (True, l, r)
